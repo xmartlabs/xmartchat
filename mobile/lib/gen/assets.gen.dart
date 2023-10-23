57e7b4402c6427840816e3fa2ee2a3cb4986cecf
Sol Mendiola <solmendiola73@gmail.com>
2023-10-23 15:35:00 -0300

    linter error

--- mobile/lib/gen/assets.gen.dart
+++ mobile/lib/gen/assets.gen.dart
@@ -17,14 +17,12 @@ class Assets {
   static const AssetGenImage logoAndNameLarge =
       AssetGenImage('assets/logo_and_name_large.png');
   static const AssetGenImage sendGreen = AssetGenImage('assets/send_green.png');
-  static const AssetGenImage videoReadme =
-      AssetGenImage('assets/video_readme.gif');
   static const AssetGenImage xlLogoSmall =
       AssetGenImage('assets/xl-logo-small.png');
 
   /// List of all assets
   List<AssetGenImage> get values =>
-      [logoAndName, logoAndNameLarge, sendGreen, videoReadme, xlLogoSmall];
+      [logoAndName, logoAndNameLarge, sendGreen, xlLogoSmall];
 }
 
 class AssetGenImage {
