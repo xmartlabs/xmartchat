eb13668e0ba63501bd50145379cb8d3861478b7b
Matías Irland <mirland@xmartlabs.com>
2023-10-25 14:59:09 +0200

    feat: Send messages when enter is pressed (#30)

--- mobile/lib/ui/home/home_screen.dart
+++ mobile/lib/ui/home/home_screen.dart
@@ -211,6 +211,8 @@ class _TextFieldSection extends StatelessWidget {
           children: [
             AppTextInputField(
               controller: textController,
+              textInputAction: TextInputAction.search,
+              onSubmitted: (_) => _submitValue(context),
               maxLines: 3,
               minLines: 1,
               trailingIcon: (textController.text.isNotEmpty)
@@ -221,11 +223,7 @@ class _TextFieldSection extends StatelessWidget {
                         color: context.theme.colors.textColor.shade100,
                       ),
                       color: context.theme.colors.primary.shade300,
-                      onPressed: () {
-                        context.read<HomeCubit>().sendMessage();
-                        textController.clear();
-                        context.read<HomeCubit>().onCurrentTextChanged("");
-                      },
+                      onPressed: () => _submitValue(context),
                     )
                   : null,
               onChanged: context.read<HomeCubit>().onCurrentTextChanged,
@@ -236,4 +234,10 @@ class _TextFieldSection extends StatelessWidget {
           ],
         ),
       );
+
+  void _submitValue(BuildContext context) {
+    context.read<HomeCubit>().sendMessage();
+    textController.clear();
+    context.read<HomeCubit>().onCurrentTextChanged("");
+  }
 }
